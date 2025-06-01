32a5e17b4b3a34cfb8808a5540e185925f62a102
Cyrus-M2 <devsben345@gmail.com>
2025-06-01 17:37:27 +0300

    Challenge one completed sum of two numbers

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -1,3 +1,8 @@
-var x;
-x = 55;
-console.log(x);
+// Challenge 1: Return sum of two numbers
+function addition(num1, num2) {
+    return num1 + num2;
+}
+var sum1 = addition(3, 5);
+var sum2 = addition(-6, 9);
+console.log(sum1); // 8
+console.log(sum2); // 3
--- milestone_1_challenges.ts
+++ milestone_1_challenges.ts
@@ -1,3 +1,8 @@
-let x : number;
-x = 55;
-console.log(x)
+// Challenge 1: Return sum of two numbers
+function addition (num1: number, num2: number) {
+    return num1 + num2;
+}
+let sum1: number = addition(3, 5);
+let sum2: number = addition(-6, 9);
+console.log(sum1); // 8
+console.log(sum2); // 3
