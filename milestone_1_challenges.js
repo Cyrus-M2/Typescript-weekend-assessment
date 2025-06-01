ff652c2528128de6bdf1f1ca0852c1045ec25d91
Cyrus-M2 <devsben345@gmail.com>
2025-06-01 17:39:22 +0300

    // Challenge 2: Convert Minutes into Seconds

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -6,3 +6,11 @@ var sum1 = addition(3, 5);
 var sum2 = addition(-6, 9);
 console.log(sum1); // 8
 console.log(sum2); // 3
+// Challenge 2: Convert Minutes into Seconds
+function convert(minutes) {
+    return minutes * 60;
+}
+var seconds1 = convert(5);
+var seconds2 = convert(2);
+console.log(seconds1); // 300
+console.log(seconds2); // 180
--- milestone_1_challenges.ts
+++ milestone_1_challenges.ts
@@ -6,3 +6,12 @@ let sum1: number = addition(3, 5);
 let sum2: number = addition(-6, 9);
 console.log(sum1); // 8
 console.log(sum2); // 3
+
+// Challenge 2: Convert Minutes into Seconds
+function convert (minutes: number) {
+    return minutes * 60;
+}
+let seconds1: number = convert(5);
+let seconds2: number = convert(2);
+console.log(seconds1); // 300
+console.log(seconds2); // 120
